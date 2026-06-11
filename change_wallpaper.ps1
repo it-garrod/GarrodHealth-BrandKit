361d982f59afb5716450d490ea961b2e6da1a4f7
it-garrod <it@garrod.ph>
2026-06-11 13:57:09 +0800

    Update change_wallpaper.ps1

--- change_wallpaper.ps1
+++ change_wallpaper.ps1
@@ -1,7 +1,15 @@
+# Path to your PNG wallpaper
+$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+$wallpaper = Join-Path $scriptDir "Wallpapers\Garrod Health Wallpaper 2026.png"
+
+# Set registry key for wallpaper
+Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name wallpaper -Value $wallpaper
+
+# Refresh the desktop to apply changes
 Add-Type @"
 using System.Runtime.InteropServices;
 
-public class Wallpaper
+public class RefreshDesktop
 {
     [DllImport("user32.dll", CharSet = CharSet.Auto)]
     public static extern int SystemParametersInfo(
@@ -13,9 +21,7 @@ public class Wallpaper
 }
 "@
 
-$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-$wallpaper = Join-Path $scriptDir "Wallpapers\Garrod Health Wallpaper 2026.png"
-
-[Wallpaper]::SystemParametersInfo(20, 0, $wallpaper, 3)
+# SPI_SETDESKWALLPAPER = 20, SPIF_UPDATEINIFILE = 0x01, SPIF_SENDCHANGE = 0x02
+[RefreshDesktop]::SystemParametersInfo(20, 0, $wallpaper, 3)
 
 Write-Host "Wallpaper changed successfully!"
