6ee79c8475b8efcbf65f9bea164ba0da143a06e8
it-garrod <it@garrod.ph>
2026-06-11 15:03:40 +0800

    Delete run_wallpaper.bat

--- run_wallpaper.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Run the PowerShell script with ExecutionPolicy bypass
-powershell.exe -ExecutionPolicy Bypass -File "%~dp0change_wallpaper.ps1"
-
-REM Keep the window open so users can see output/errors
-pause
